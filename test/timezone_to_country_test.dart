8f2e1f1271dabb5ee4c55d2aa5063aaf99b1b42a
Kyle Seongwoo Jun <njk230@gmail.com>
2021-01-09 15:54:29 +0900

    [test] Add test code

--- test/timezone_to_country_test.dart
+++ test/timezone_to_country_test.dart
@@ -3,11 +3,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:timezone_to_country/timezone_to_country.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  test('translates timezone id to country code', () {
+    expect(TimeZoneToCountry.countryCode('Asia/Seoul'), 'KR');
+    expect(TimeZoneToCountry.countryCode('America/Los_Angeles'), 'US');
+    expect(TimeZoneToCountry.countryCode('Europe/London'), 'GB');
+    expect(TimeZoneToCountry.countryCode('Wrong/Time_Zone'), null);
+    expect(TimeZoneToCountry.countryCode(null), null);
   });
 }
