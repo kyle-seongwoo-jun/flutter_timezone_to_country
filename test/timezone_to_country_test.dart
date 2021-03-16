66a288de7185393fb4a20a49219b66ae73b04221
Kyle Seongwoo Jun <njk230@gmail.com>
2021-03-16 13:10:48 +0900

    [test] Update test code

--- test/timezone_to_country_test.dart
+++ test/timezone_to_country_test.dart
@@ -8,40 +8,44 @@ void main() {
   const String NOT_FOUND = 'NOT_FOUND';
   final Matcher throwsCountryNotFoundException = throwsA(isA<CountryNotFoundException>());
 
-  test('translates timezone id to country code', () {
-    expect(TimeZoneToCountry.getCountryCode('Asia/Seoul'), 'KR');
-    expect(TimeZoneToCountry.getCountryCode('America/Los_Angeles'), 'US');
-    expect(TimeZoneToCountry.getCountryCode('Europe/London'), 'GB');
+  group('TimeZoneToCountry', () {
+    test('translates timezone id to country code', () {
+      expect(TimeZoneToCountry.getCountryCode('Asia/Seoul'), 'KR');
+      expect(TimeZoneToCountry.getCountryCode('America/Los_Angeles'), 'US');
+      expect(TimeZoneToCountry.getCountryCode('Europe/London'), 'GB');
 
-    expect(() => TimeZoneToCountry.getCountryCode('Wrong/Time_Zone'), throwsCountryNotFoundException);
-    expect(() => TimeZoneToCountry.getCountryCode('GMT'), throwsCountryNotFoundException);
-    expect(() => TimeZoneToCountry.getCountryCode('UTC'), throwsCountryNotFoundException);
+      expect(() => TimeZoneToCountry.getCountryCode('Wrong/Time_Zone'), throwsCountryNotFoundException);
+      expect(() => TimeZoneToCountry.getCountryCode('GMT'), throwsCountryNotFoundException);
+      expect(() => TimeZoneToCountry.getCountryCode('UTC'), throwsCountryNotFoundException);
 
-    expect(TimeZoneToCountry.getCountryCode('Wrong/Time_Zone', onNotFound: () => NOT_FOUND), NOT_FOUND);
-    expect(TimeZoneToCountry.getCountryCode('GMT', onNotFound: () => NOT_FOUND), NOT_FOUND);
-    expect(TimeZoneToCountry.getCountryCode('UTC', onNotFound: () => NOT_FOUND), NOT_FOUND);
+      expect(TimeZoneToCountry.getCountryCode('Wrong/Time_Zone', onNotFound: () => NOT_FOUND), NOT_FOUND);
+      expect(TimeZoneToCountry.getCountryCode('GMT', onNotFound: () => NOT_FOUND), NOT_FOUND);
+      expect(TimeZoneToCountry.getCountryCode('UTC', onNotFound: () => NOT_FOUND), NOT_FOUND);
+    });
+
+    test('gets country code from [Location]', () async {
+      tz.initializeTimeZones();
+
+      expect(tz.getLocation('Asia/Seoul').countryCode, 'KR');
+      expect(tz.getLocation('America/Los_Angeles').countryCode, 'US');
+      expect(tz.getLocation('Europe/London').countryCode, 'GB');
+
+      expect(() => tz.getLocation('US/Central').countryCode, throwsCountryNotFoundException);
+      expect(() => tz.getLocation('US/Eastern').countryCode, throwsCountryNotFoundException);
+      expect(() => tz.getLocation('US/Pacific').countryCode, throwsCountryNotFoundException);
+
+      expect(tz.getLocation('US/Central').getCountryCode(onNotFound: () => NOT_FOUND), NOT_FOUND);
+      expect(tz.getLocation('US/Eastern').getCountryCode(onNotFound: () => NOT_FOUND), NOT_FOUND);
+      expect(tz.getLocation('US/Pacific').getCountryCode(onNotFound: () => NOT_FOUND), NOT_FOUND);
+    });
   });
 
-  test('gets country code from [Location]', () async {
-    tz.initializeTimeZones();
-
-    expect(tz.getLocation('Asia/Seoul').countryCode, 'KR');
-    expect(tz.getLocation('America/Los_Angeles').countryCode, 'US');
-    expect(tz.getLocation('Europe/London').countryCode, 'GB');
-
-    expect(() => tz.getLocation('US/Central').countryCode, throwsCountryNotFoundException);
-    expect(() => tz.getLocation('US/Eastern').countryCode, throwsCountryNotFoundException);
-    expect(() => tz.getLocation('US/Pacific').countryCode, throwsCountryNotFoundException);
-
-    expect(tz.getLocation('US/Central').getCountryCode(onNotFound: () => NOT_FOUND), NOT_FOUND);
-    expect(tz.getLocation('US/Eastern').getCountryCode(onNotFound: () => NOT_FOUND), NOT_FOUND);
-    expect(tz.getLocation('US/Pacific').getCountryCode(onNotFound: () => NOT_FOUND), NOT_FOUND);
-  });
-
-  test('Unsupported [Location]', () async {
-    tz.initializeTimeZones();
-    tz.timeZoneDatabase.locations.entries
-        .where((location) => location.value.getCountryCode(onNotFound: () => NOT_FOUND) == NOT_FOUND)
-        .forEach((location) => print(location.key));
+  group('to check', () {
+    test('Unsupported [Location]', () async {
+      tz.initializeTimeZones();
+      tz.timeZoneDatabase.locations.entries
+          .where((location) => location.value.getCountryCode(onNotFound: () => NOT_FOUND) == NOT_FOUND)
+          .forEach((location) => print(location.key));
+    });
   });
 }
