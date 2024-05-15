e92d910b2d99adba211ec4e1d625a48616ff2952
Kyle Seongwoo Jun <njk230@gmail.com>
2024-05-15 22:39:44 +0900

    chore(example): improve exmaple

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,53 +3,40 @@ import 'package:timezone_to_country/timezone_to_country.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _countryCode = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    _initData();
-  }
-
-  Future<void> _initData() async {
-    try {
-      _countryCode = await TimeZoneToCountry.getLocalCountryCode();
-    } catch (e) {
-      print('Could not get the local country code');
-    }
-    if (mounted) {
-      setState(() {});
-    }
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Local country code app'),
+        appBar: AppBar(
+          title: const Text('Local country code app'),
+        ),
+        body: Center(
+          child: FutureBuilder<String>(
+            future: TimeZoneToCountry.getLocalCountryCode(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator.adaptive();
+              }
+              if (snapshot.hasError) {
+                return const Text('Could not get the local country code');
+              }
+
+              String countryCode = snapshot.data!;
+              return Text(
+                'Local country code: ${countryCode} ${EmojiUtils.country(countryCode)}',
+                style: Theme.of(context).textTheme.headlineSmall,
+              );
+            },
           ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Local country code: ${convertToEmoji(_countryCode)}',
-                  style: Theme.of(context).textTheme.headlineSmall,
-                ),
-              ],
-            ),
-          )),
+        ),
+      ),
     );
   }
+}
 
-  static String convertToEmoji(String countryCode) {
+class EmojiUtils {
+  static String country(String countryCode) {
     String flag = countryCode.replaceAllMapped(RegExp(r'[A-Z]'),
         (match) => String.fromCharCode(match.group(0)!.codeUnitAt(0) + 127397));
     return flag;
